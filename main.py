2b08bb2075dbf22099dc0490256e2abffff4ca10
Melnyk Vladislav <vm200891@gmail.com>
2024-03-28 09:25:07 +0200

    delete config

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
+from config import telegram_bot_token
+
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -13,7 +15,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('6954012675:AAGfOjkly8fukjCrIRpWzSYAxUM-uaOgUzc').build()
+    application = ApplicationBuilder().token(telegram_bot_token).build()
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
