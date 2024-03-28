4c33195285e6ed4397d9dbe659be6014fdc1a833
Melnyk Vladislav <vm200891@gmail.com>
2024-03-28 14:16:25 +0200

    using logging

--- loging_example.py
+++ loging_example.py
@@ -1,1 +1,12 @@
-#
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
+
+logging.debug('debug message')
+logging.info('info message')
+logging.warning('warnig massage')
+logging.error('error message')
+logging.critical('critical message')
