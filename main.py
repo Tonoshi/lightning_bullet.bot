5dc8ee53df4b16afdd818fa1ec590f5304726a76
Melnyk Vladislav <vm200891@gmail.com>
2024-03-28 15:20:09 +0200

    done

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
-from config import telegram_bot_token
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from dotenv import load_dotenv
+import logging
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    level=logging.DEBUG
+
 )
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Здоров {update.effective_user.first_name}')
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    massage = update.message.text.lower()
+    if 'привіт' in massage:
+        reply_text = f"Доброго дня {update.effective_user.first_name}"
+    else:
+        reply_text = 'Я тебе не зрозумів.'
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="holoooo")
+    await update.message.reply_text(reply_text)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Start {update.effective_user.first_name}')
+app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+app.add_handler(CommandHandler("Hello", hello))
+app.add_handler(CommandHandler("Start", start))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(telegram_bot_token).build()
-
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-
-    application.run_polling()
+app.run_polling()
